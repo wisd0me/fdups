0663ba77e33e83c6de27b7893f364c03e80d55ff
wisd0me <ak47izatool@gmail.com>
2016-03-20 18:34:27 +0600

    command line arguments added

--- fdups.py
+++ fdups.py
@@ -1,10 +1,6 @@
-#!env python3                                                                                                                                              [9/739]
-
-infile = 'dups.log'
-outfile = infile + '.out'
-encoding = 'utf8'
-filter_size_lt = 49000000 # filter out sizes less than this
-############################################################
+#!env python3
+import sys
+import argparse
 
 SUFFIXES = {1000: ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
             1024: ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']}
@@ -31,33 +27,63 @@ def approximate_size(size, a_kilobyte_is_1024_bytes=True):
 
     raise ValueError('number too large')
 
-# parsed format looks like this:
-# 31337 bytes each:
-# /path/to/dup1
-# /path/to/dup2
-# <newline>
-# 31338 bytes each:
-# ... and so on
+def process(args):
+    '''
+    Parses fdupes log, created with option -S, and converts file sizes in bytes
+    into human readable sizes.
 
-input = open(infile, mode='r', encoding=encoding, errors='replace')
-output = open(outfile, mode='w', encoding=encoding)
+    Input log format looks like this:
+    31337 bytes each:
+    /path/to/dup1
+    /path/to/dup2
+    <newline>
+    31338 bytes each:
+    ... and so on
 
-for line in input:
+    '''
+    input = open(args.input, mode='r', encoding=args.encoding, errors='replace')
+    output = open(args.output, mode='w', encoding=args.encoding)
+    filter_size = int(args.limit)
+
+    for line in input:
         if line.find(' bytes each:') ==  -1:
-                continue
+            continue
 
         #print(line, end='')
         tokens = line.split(' ')
         # filter reports which is below the specified bound
-        if int(tokens[0]) < filter_size_lt:
-                for skipln in input:
-                        if skipln == '\n':
-                                break
+        dups_size = int(tokens[0])
+        if dups_size < filter_size:
+            for skipln in input:
+                if skipln == '\n':
+                    break
         else:
-                line = approximate_size(int(tokens[0])) + ' each:\n'
-                output.write(line)
+            line = approximate_size(dups_size) + ' each:\n'
+            output.write(line)
 
-                for writeln in input:
-                        output.write(writeln)
-                        if writeln == '\n':
-                                break
+            for writeln in input:
+                output.write(writeln)
+                if writeln == '\n':
+                    break
+
+    input.close()
+    output.close()
+
+
+# known arguments
+argp = argparse.ArgumentParser(description='processes fdupes utility log file')
+argp.add_argument('-i', '--input', dest='input', required=True,
+                  help='fdupes log input file; must be created with -S option')
+argp.add_argument('-o', '--output', dest='output',
+                  help='output file; if omitted, \'.out\' suffix will be appended to input file name')
+argp.add_argument('-l', '--limit', dest='limit', default=0,
+                  help='records with size less than given will be ignored')
+argp.add_argument('-e', '--encoding', dest='encoding', default='utf8',
+                  help='file encoding; by default, utf8 is used')
+
+args = argp.parse_args()
+
+if (args.output == None):
+    args.output = args.input + '.out'
+
+process(args)
